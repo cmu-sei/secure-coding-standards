943fed8996be5e35353b7a19a0a2b27ab0be923c
Jason D Shimkoski <jdshimkoski@sei.cmu.edu>
2026-03-16 09:38:15 -0400

    add robots.txt and add nuxt config options to ensure no indexing occurs until we are ready for that to begin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   app: {
-    baseURL: '/secure-coding-standards/'
+    baseURL: '/secure-coding-standards/',
+    // TODO: Remove the following meta tag once the site is ready to be indexed by search engines
+    head: {
+      meta: [
+        { name: 'robots', content: 'noindex, nofollow' }
+      ]
+    }
   }
 })
