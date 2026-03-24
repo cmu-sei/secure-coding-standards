495173d7e8eb110ccd112ec105df1e743fb13b3d
Richard Laughlin <rwlaughlin@cert.org>
2026-03-24 13:59:36 -0400

    Add fortran coding standard extracts.
    Currently the ordering is left at 4 (even though there is already
    another 4) because otherwise every file in the entire content repo gets
    renamed and this will create confusing diffs later. IMO It's better to leave
    it at 4 until we're closer to merge with `main`.

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,23 +1,24 @@
 export default defineAppConfig({
-  sitemap: {
-    hostname: 'https://cmu-sei.github.io/secure-coding-standards/',
-  },
-  githubUrl: 'https://github.com/cmu-sei/secure-coding-standards',
-  appSuitePrefix: 'SEI',
-  appSuite: 'CERT',
-  pageTitle: 'CERT Secure Coding',
-  navigation: [
-    { title: 'Home', path: '/' },
-    {
-      title: 'Coding Standards',
-      path: '/coding-standards',
-      children: [
-        { title: 'Android Coding Standard', path: '/android-secure-coding-standard' },
-        { title: 'C Coding Standard', path: '/sei-cert-c-coding-standard' },
-        { title: 'C++ Coding Standard', path: '/sei-cert-cpp-coding-standard' },
-        { title: 'Java Coding Standard', path: '/sei-cert-oracle-coding-standard-for-java' },
-        { title: 'Perl Coding Standard', path: '/sei-cert-perl-coding-standard' },
-      ]
+    sitemap: {
+        hostname: 'https://cmu-sei.github.io/secure-coding-standards/',
     },
-  ],
+    githubUrl: 'https://github.com/cmu-sei/secure-coding-standards',
+    appSuitePrefix: 'SEI',
+    appSuite: 'CERT',
+    pageTitle: 'CERT Secure Coding',
+    navigation: [
+        { title: 'Home', path: '/' },
+        {
+            title: 'Coding Standards',
+            path: '/coding-standards',
+            children: [
+                { title: 'Android Coding Standard', path: '/android-secure-coding-standard' },
+                { title: 'C Coding Standard', path: '/sei-cert-c-coding-standard' },
+                { title: 'C++ Coding Standard', path: '/sei-cert-cpp-coding-standard' },
+                { title: 'Fortran Coding Standard', path: '/cert-fortran-secure-coding-standard' },
+                { title: 'Java Coding Standard', path: '/sei-cert-oracle-coding-standard-for-java' },
+                { title: 'Perl Coding Standard', path: '/sei-cert-perl-coding-standard' },
+            ]
+        },
+    ],
 })
