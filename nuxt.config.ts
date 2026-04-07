43a00474589695a5591fd2839929ddc24da0460e
Jason D Shimkoski <jdshimkoski@sei.cmu.edu>
2026-04-07 09:26:57 -0400

    remove robots.txt and robots meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,5 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     baseURL: '/secure-coding-standards/',
-    // TODO: Remove the following meta tag once the site is ready to be indexed by search engines
-    head: {
-      meta: [
-        { name: 'robots', content: 'noindex, nofollow' }
-      ]
-    }
   }
 })
